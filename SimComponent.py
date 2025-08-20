3839ed4700fff93f524891639cc6b2b91d479b5e
Megan Leigh <93282848+meganleigh0@users.noreply.github.com>
2025-08-20 10:03:33 -0400

    Update SimComponent.py

--- SimComponent.py
+++ SimComponent.py
@@ -1,10 +1,1 @@
-Alignment
-
-Hi team,
-
-I’ve attached the process steps I documented for updating the MRR. From what I can tell, though, this has been an ongoing effort since last year, and it sounds like the new report with the additional columns has already been created in the testing environment. It seems that the next step is simply to test it.
-
-I just wanted to check in and make sure we’re all on the same page, and to see if there’s anything I can do to help support this effort—whether that’s assisting with testing or helping to finalize the report in Cognos.
-
-Thanks,
-Me
+l
